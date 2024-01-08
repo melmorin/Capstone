9514bc1af60adb4a40bf3320e76be25e04c52ebe
melmorin <77110440+melmorin@users.noreply.github.com>
2024-01-08 14:29:40 -0500

    1/8/24

--- Assets/Scripts/Level Scripts/MoveBackground.cs
+++ Assets/Scripts/Level Scripts/MoveBackground.cs
@@ -7,7 +7,8 @@ public class MoveBackground : MonoBehaviour
     private float length; 
     private float startPos;
     private GameObject mainCam; 
-    private bool canMove = true;
+    private EdgeCollider2D edges; 
+    private BoxCollider2D backgroundEdge; 
     [SerializeField] private float parallaxEffect; 
 
     // Start is called before the first frame update
@@ -16,26 +17,25 @@ public class MoveBackground : MonoBehaviour
         mainCam = GameObject.Find("Virtual Camera");
         startPos = transform.position.x; 
         length = gameObject.GetComponent<SpriteRenderer>().bounds.size.x; 
+        edges = GameObject.Find("World Edge").GetComponent<EdgeCollider2D>();
+        backgroundEdge = gameObject.GetComponent<BoxCollider2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canMove)
+        float temp = (mainCam.transform.position.x * (1 - parallaxEffect)); 
+
+        float distance = (mainCam.transform.position.x * parallaxEffect); 
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
         {
-            float temp = (mainCam.transform.position.x * (1 - parallaxEffect)); 
-
-            float distance = (mainCam.transform.position.x * parallaxEffect); 
-            transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
-
-            if (temp > startPos + length)
-            {
-                startPos += length;
-            }
-            else if (temp < startPos - length)
-            {
-                startPos -= length;
-            }
+            startPos += length;
+        }
+        else if (temp < startPos - length)
+        {
+            startPos -= length;
         }
     }
 }
--- Assets/Scripts/Level Scripts/PlayerController.cs
+++ Assets/Scripts/Level Scripts/PlayerController.cs
@@ -240,7 +240,6 @@ public class PlayerController : MonoBehaviour
     // Runs when the player exits a collider 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Yeehaw");
         if (collision.gameObject.tag == "Platform")
         {
             Vector3 hit = collision.contacts[0].normal;
