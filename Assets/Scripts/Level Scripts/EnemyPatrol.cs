056c61afc95a8cc6137fd34943ae144b42338b63
melmorin <77110440+melmorin@users.noreply.github.com>
2024-03-04 14:25:33 -0500

    3/4/23

--- Assets/Scripts/Level Scripts/EnemyPatrol.cs
+++ Assets/Scripts/Level Scripts/EnemyPatrol.cs
@@ -8,12 +8,14 @@ public class EnemyPatrol : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f; 
     private Rigidbody2D rigb;
     private GameManager gameManager; 
+    private Animator anim; 
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.FindGameObjectWithTag("Game_Manager").GetComponent<GameManager>();
         rigb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -30,7 +32,16 @@ public class EnemyPatrol : MonoBehaviour
                 rigb.velocity = new Vector2(-moveSpeed, 0f);
             }
         }
-        else rigb.velocity = new Vector2(0, 0f);
+    }
+
+    // Ends Animation at certain point if game is over
+    void EndAnim()
+    {
+        if (gameManager.gameOver)
+        {
+            anim.speed = 0; 
+            rigb.velocity = new Vector2(0, 0f);
+        }
     }
 
     // Move enemy when hitting an edge  
