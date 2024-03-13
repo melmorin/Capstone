8d87f5e6f14772703b035ea166b68daa446cac93
melmorin <77110440+melmorin@users.noreply.github.com>
2024-03-13 14:25:20 -0400

    3/13/24

--- Assets/Scripts/Hub Scripts/DialogueController.cs
+++ Assets/Scripts/Hub Scripts/DialogueController.cs
@@ -23,11 +23,25 @@ public class DialogueController : MonoBehaviour
     public int index = 0;  
     public string[] currentDialogue;
     public Coroutine typingRoutine; 
+    public GameObject currentNPC;  
 
     // Adds listener to the continue button for NextLine()
     void Awake()
     {
         continueButton.GetComponent<Button>().onClick.AddListener(Nextline);
+    }
+
+    // Updates every frame 
+    void Update()
+    {
+        if (currentDialogue.Length - 1 >= index)
+        {
+            if (dialogueText.text == currentDialogue[index])
+            {
+                continueButton.SetActive(true); 
+                skipButton.SetActive(false); 
+            }
+        }
     }
 
     // Moves onto the next line of dialogue or closes window 
@@ -45,6 +59,19 @@ public class DialogueController : MonoBehaviour
         {
             ZeroText(); 
         }
+    }
+
+    // Begins coroutine for the BPC script
+    public void StartTypingRoutine()
+    {
+        typingRoutine = StartCoroutine(Typing()); 
+    }
+
+    // checks if equal
+    public bool IsEqual()
+    {
+        if (index == currentDialogue.Length - 1) return true; 
+        else return false;  
     }
 
     // Stops the typing animation if the player skips the text
@@ -67,7 +94,9 @@ public class DialogueController : MonoBehaviour
     // Closes down entire dialogue menu 
     public void ZeroText()
     {
+        StopCoroutine(typingRoutine); 
         dialogueText.text = "";
+        if (index == currentDialogue.Length - 1) currentNPC.GetComponent<NPC>().readOnce = true; 
         index = 0; 
         if (dialoguePanel != null)
         {
--- Assets/Scripts/Hub Scripts/NPC.cs
+++ Assets/Scripts/Hub Scripts/NPC.cs
@@ -17,7 +17,9 @@ public class NPC : MonoBehaviour
     private bool playerIsClose;
     private DialogueController dialogueController;
     private GameManager gameManager;
-    private bool readOnce = false; 
+
+    [Header ("Runtime Vars")]
+    public bool readOnce = false; 
 
     // Runs before the first frame 
     void Start()
@@ -35,7 +37,6 @@ public class NPC : MonoBehaviour
         {
             if (dialogueController.dialoguePanel.activeInHierarchy)
             {
-                StopCoroutine(dialogueController.typingRoutine);
                 dialogueController.ZeroText(); 
             }
             else 
@@ -44,32 +45,9 @@ public class NPC : MonoBehaviour
                 if (readOnce) dialogueController.currentDialogue = secondDialogue; 
                 else dialogueController.currentDialogue = dialogue; 
                 dialogueController.profileImage.sprite = npcProfile; 
+                dialogueController.currentNPC = gameObject; 
                 dialogueController.nameText.text = npcName; 
-                dialogueController.typingRoutine = StartCoroutine(dialogueController.Typing());
-                while (dialogueController.dialoguePanel.activeInHierarchy) 
-                {
-                    if (dialogueController.index !< dialogueController.currentDialogue.Length - 1)
-                    {
-                        readOnce = true; 
-                    }
-                }
-            }
-        }
-
-        if (dialogue.Length - 1 >= dialogueController.index && !readOnce)
-        {
-            if (dialogueController.dialogueText.text == dialogue[dialogueController.index])
-            {
-                dialogueController.continueButton.SetActive(true); 
-                dialogueController.skipButton.SetActive(false); 
-            }
-        }
-        else if (secondDialogue.Length - 1 >= dialogueController.index && readOnce)
-        {
-            if (dialogueController.dialogueText.text == secondDialogue[dialogueController.index])
-            {
-                dialogueController.continueButton.SetActive(true); 
-                dialogueController.skipButton.SetActive(false); 
+                dialogueController.StartTypingRoutine();
             }
         }
     }
